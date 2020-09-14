fbcf4ddbbc5e524f502ad286499bb705007a82b3
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-09-14 16:10:23 +0200

    Update Program.cs

--- Pond/Program.cs
+++ Pond/Program.cs
@@ -33,38 +33,64 @@ namespace Pond
             else if (a.Keyless[0] == "init")
             {
                 // Initialize new Pond project
-                Console.Write($"Are you sure you want to create a new project in '{Directory.GetCurrentDirectory()}'? ");
-                string ans = Console.ReadLine().ToLower();
-                if (ans == "yes" || ans == "y")
+                #region Initialize
+
+                string cd = Directory.GetCurrentDirectory();
+                if (Confirm($"Are you sure you want to create a new project in '{cd}'? "))
                 {
-                    string json = JsonConvert.SerializeObject(Config.Default, Formatting.Indented);
-                    string configPath = Path.Combine(Directory.GetCurrentDirectory(), "config.json");
-                    Console.WriteLine($"Creating {configPath}...");
-                    File.WriteAllText(configPath, json);
-                    string buildPath = Path.Combine(Directory.GetCurrentDirectory(), "build.bat");
+                    bool useConf = Confirm("Do you need a custom config file? ");
+                    string configPath = null;
+                    if (useConf)
+                    {
+                        string json = JsonConvert.SerializeObject(Config.Default, Formatting.Indented);
+                        configPath = Path.Combine(cd, "config.json");
+                        Console.WriteLine($"Creating {configPath}...");
+                        File.WriteAllText(configPath, json);
+                    }
+
+                    string buildPath = Path.Combine(cd, "build.bat");
                     Console.WriteLine($"Creating {buildPath}...");
                     File.WriteAllText(buildPath, $@"@echo off
-Pond build ""{configPath}""
+Pond build{( useConf ? $" \"{configPath}\"" : "" )}
 pause
 ");
                     Console.WriteLine("Done! Project has successfully been initialize.", ConsoleColor.Green);
                 }
                 else Console.WriteLine("Project canceled.");
+
+                #endregion
             }
             else if (a.Keyless[0] == "build")
             {
                 // Run Pond
+                #region Build
+
                 Console.WriteLine("Building project...", ConsoleColor.Yellow);
                 Config config = LoadConfig(a);
+
                 Console.WriteLine("Compiling...", ConsoleColor.Yellow);
                 PondCompiler.Compile(config);
-
-
                 
                 Console.WriteLine("Done! Website has successfully been built.", ConsoleColor.Green);
+
+                #endregion
             }
             else Console.WriteLine("Unknown command, please use /? or /h for more help.", ConsoleColor.Red);
         }
+
+        static bool Prompt(string message, bool ignoreCase, params string[] acceptedInput)
+        {
+            Console.Write(message);
+            string ans = Console.ReadLine();
+            if (ignoreCase) ans = ans.ToLower();
+            for (int i = 0; i < acceptedInput.Length; i++)
+            {
+                if (acceptedInput[i].Equals(ans)) return true;
+            }
+
+            return false;
+        }
+        static bool Confirm(string message) => Prompt(message, true, "yes", "y", "yep", "yup", "ok");
 
         static Config LoadConfig(Arguments a)
         {
